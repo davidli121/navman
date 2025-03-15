7512207a86b2788cf1b9472eb5db859ed2f6d9b9
dli <124467536+dli152@users.noreply.github.com>
2025-03-15 13:40:51 -0400

    Update main.js

--- pg/main.js
+++ pg/main.js
@@ -2,3 +2,21 @@ function startdone() {
     document.getElementById("startsc").hidden = true
     document.getElementById("loginscreen").hidden = false
 }
+function testRequestToLocalHost() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:3000/testreq');
+    request.onload = function() {
+        if (request.status === 200) {
+            document.getElementById("startsc").hidden = true
+            document.getElementById("loginscreen").hidden = false
+        } else {
+            document.getElementById("startsc").hidden = true
+            document.getElementById("error").hidden = false
+        }
+    };
+    request.onerror = function() {
+        document.getElementById("startsc").hidden = true
+        document.getElementById("error").hidden = false
+    };
+    request.send();
+}
